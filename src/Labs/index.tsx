cc97556c4642b8e797d23280f59897115ce288a7
Rachael <schlosberg.r@northeastern.edu>
2024-10-29 21:13:54 -0400

    Lab 4

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,20 +3,27 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
 import Kanbas from "../Kanbas";
+import store from "./store";
+import { Provider } from "react-redux";
+
 export default function Labs() {
   return (
-    <div>
-      <h1>Rachael Schlosberg</h1>
-      <h3>CS 5610 | Web Development | Section: 03 | CRN: 20596 | Online</h3>
+    <Provider store={store}>
+      <div>
+        <h1>Rachael Schlosberg</h1>
+        <h3>CS 5610 | Web Development | Section: 03 | CRN: 20596 | Online</h3>
 
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Kanbas" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-      </Routes>
-    </div>
+        <TOC />
+        <Routes>
+          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4/*" element={<Lab4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
